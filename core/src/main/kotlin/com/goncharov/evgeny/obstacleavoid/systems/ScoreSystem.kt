90a5f2501b24a74acfdac443998b871245a7abf7
Evgeny Goncharov <jtgn@yandex.ru>
2022-07-03 17:50:45 +0300

    реализация UiRenderSystem и ScoreSystem

--- core/src/main/kotlin/com/goncharov/evgeny/obstacleavoid/systems/ScoreSystem.kt
+++ core/src/main/kotlin/com/goncharov/evgeny/obstacleavoid/systems/ScoreSystem.kt
@@ -1,13 +1,15 @@
 package com.goncharov.evgeny.obstacleavoid.systems
 
 import com.badlogic.ashley.systems.IntervalSystem
+import com.badlogic.gdx.math.MathUtils
 import com.goncharov.evgeny.obstacleavoid.consts.SCORE_MAX_TIME
+import com.goncharov.evgeny.obstacleavoid.managers.GameManager
 
 class ScoreSystem : IntervalSystem(
     SCORE_MAX_TIME
 ) {
 
     override fun updateInterval() {
-
+        GameManager.updateScore(MathUtils.random(1, 5))
     }
 }
--- core/src/main/kotlin/com/goncharov/evgeny/obstacleavoid/systems/UiRenderSystem.kt
+++ core/src/main/kotlin/com/goncharov/evgeny/obstacleavoid/systems/UiRenderSystem.kt
@@ -1,9 +1,15 @@
 package com.goncharov.evgeny.obstacleavoid.systems
 
 import com.badlogic.ashley.core.EntitySystem
+import com.badlogic.gdx.graphics.Color
 import com.badlogic.gdx.graphics.g2d.BitmapFont
+import com.badlogic.gdx.graphics.g2d.GlyphLayout
 import com.badlogic.gdx.graphics.g2d.SpriteBatch
 import com.badlogic.gdx.utils.viewport.Viewport
+import com.goncharov.evgeny.obstacleavoid.consts.UI_HEIGHT
+import com.goncharov.evgeny.obstacleavoid.consts.UI_WIDTH
+import com.goncharov.evgeny.obstacleavoid.managers.GameManager
+import com.goncharov.evgeny.obstacleavoid.screens.game.GameRender
 
 class UiRenderSystem(
     private val font: BitmapFont,
@@ -11,4 +17,28 @@ class UiRenderSystem(
     private val batch: SpriteBatch
 ) : EntitySystem() {
 
+    private val layout = GlyphLayout()
+
+    override fun update(deltaTime: Float) {
+        uiViewport.apply()
+        batch.projectionMatrix = uiViewport.camera.combined
+        batch.begin()
+        draw()
+        batch.end()
+    }
+
+    private fun draw() {
+        val liveText = LIVE_TEXT.format(GameManager.lives)
+        layout.setText(font, liveText)
+        font.color = Color.WHITE
+        font.draw(batch, liveText, 20f, UI_HEIGHT - layout.height)
+        val scoreText = SCORE_TEXT.format(GameManager.score)
+        layout.setText(font, scoreText)
+        font.draw(batch, scoreText, UI_WIDTH - layout.width - 20f, UI_HEIGHT - layout.height)
+    }
+
+    companion object {
+        private const val LIVE_TEXT = "LIVES: %d"
+        private const val SCORE_TEXT = "SCORE: %d"
+    }
 }
