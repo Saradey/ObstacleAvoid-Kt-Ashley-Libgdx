e46f9b9012a142499bfc61f8c5ba812670ef3c0d
Evgeny Goncharov <jtgn@yandex.ru>
2022-07-03 17:01:56 +0300

    реализация WorldWrapSystem

--- core/src/main/kotlin/com/goncharov/evgeny/obstacleavoid/systems/WorldWrapSystem.kt
+++ core/src/main/kotlin/com/goncharov/evgeny/obstacleavoid/systems/WorldWrapSystem.kt
@@ -3,7 +3,9 @@ package com.goncharov.evgeny.obstacleavoid.systems
 import com.badlogic.ashley.core.Entity
 import com.badlogic.ashley.core.Family
 import com.badlogic.ashley.systems.IteratingSystem
+import com.badlogic.gdx.math.MathUtils
 import com.badlogic.gdx.utils.viewport.Viewport
+import com.goncharov.evgeny.obstacleavoid.common.Mappers
 import com.goncharov.evgeny.obstacleavoid.components.DimensionComponent
 import com.goncharov.evgeny.obstacleavoid.components.PositionComponent
 import com.goncharov.evgeny.obstacleavoid.components.WorldWrapComponent
@@ -19,6 +21,9 @@ class WorldWrapSystem(
 ) {
 
     override fun processEntity(entity: Entity, deltaTime: Float) {
-
+        val position = Mappers.position[entity]
+        val dimension = Mappers.dimension[entity]
+        position.x = MathUtils.clamp(position.x, 0f, viewport.worldWidth - dimension.width)
+        position.y = MathUtils.clamp(position.y, 0f, viewport.worldHeight - dimension.height)
     }
 }
