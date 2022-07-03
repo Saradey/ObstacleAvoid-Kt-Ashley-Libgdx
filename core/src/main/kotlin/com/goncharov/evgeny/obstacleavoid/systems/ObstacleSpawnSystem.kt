925aabb2fc880ff922238ac3e156eacd3966789d
Evgeny Goncharov <jtgn@yandex.ru>
2022-07-03 17:30:42 +0300

    реализация ObstacleSpawnSystem

--- core/src/main/kotlin/com/goncharov/evgeny/obstacleavoid/systems/ObstacleSpawnSystem.kt
+++ core/src/main/kotlin/com/goncharov/evgeny/obstacleavoid/systems/ObstacleSpawnSystem.kt
@@ -1,8 +1,12 @@
 package com.goncharov.evgeny.obstacleavoid.systems
 
 import com.badlogic.ashley.systems.IntervalSystem
+import com.badlogic.gdx.math.MathUtils
 import com.goncharov.evgeny.obstacleavoid.common.EntityFactory
+import com.goncharov.evgeny.obstacleavoid.consts.OBSTACLE_SIZE
 import com.goncharov.evgeny.obstacleavoid.consts.OBSTACLE_SPAWN_TIME
+import com.goncharov.evgeny.obstacleavoid.consts.WORLD_HEIGHT
+import com.goncharov.evgeny.obstacleavoid.consts.WORLD_WIDTH
 
 class ObstacleSpawnSystem(
     private val factory: EntityFactory
@@ -11,6 +15,8 @@ class ObstacleSpawnSystem(
 ) {
 
     override fun updateInterval() {
-
+        val min = 0f
+        val max = WORLD_WIDTH - OBSTACLE_SIZE
+        factory.addObstacle(MathUtils.random(min, max), WORLD_HEIGHT)
     }
 }
