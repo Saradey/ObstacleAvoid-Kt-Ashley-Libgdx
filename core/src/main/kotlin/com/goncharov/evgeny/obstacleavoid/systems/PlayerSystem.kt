66b1c7ed89c1e647724f9a530e7ee547a01b3291
Evgeny Goncharov <jtgn@yandex.ru>
2022-07-03 16:50:33 +0300

    написали PlayerSystem

--- core/src/main/kotlin/com/goncharov/evgeny/obstacleavoid/systems/PlayerSystem.kt
+++ core/src/main/kotlin/com/goncharov/evgeny/obstacleavoid/systems/PlayerSystem.kt
@@ -3,8 +3,12 @@ package com.goncharov.evgeny.obstacleavoid.systems
 import com.badlogic.ashley.core.Entity
 import com.badlogic.ashley.core.Family
 import com.badlogic.ashley.systems.IteratingSystem
+import com.badlogic.gdx.Gdx
+import com.badlogic.gdx.Input
+import com.goncharov.evgeny.obstacleavoid.common.Mappers
 import com.goncharov.evgeny.obstacleavoid.components.MovementComponent
 import com.goncharov.evgeny.obstacleavoid.components.PlayerComponent
+import com.goncharov.evgeny.obstacleavoid.consts.MAX_PLAYER_X_SPEED
 
 class PlayerSystem : IteratingSystem(
     Family.all(
@@ -14,6 +18,15 @@ class PlayerSystem : IteratingSystem(
 ) {
 
     override fun processEntity(entity: Entity, deltaTime: Float) {
-
+        when {
+            Gdx.input.isKeyPressed(Input.Keys.RIGHT) -> {
+                val movement = Mappers.movement[entity]
+                movement.xSpeed = MAX_PLAYER_X_SPEED
+            }
+            Gdx.input.isKeyPressed(Input.Keys.LEFT) -> {
+                val movement = Mappers.movement[entity]
+                movement.xSpeed = -MAX_PLAYER_X_SPEED
+            }
+        }
     }
 }
