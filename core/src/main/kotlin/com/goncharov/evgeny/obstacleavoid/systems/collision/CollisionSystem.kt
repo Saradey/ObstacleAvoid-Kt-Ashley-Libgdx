c0d9dd23a0cdb7d5bb8c0caa13e5b1317203e712
Evgeny Goncharov <jtgn@yandex.ru>
2022-07-03 17:42:03 +0300

    реализация CollisionSystem

--- core/src/main/kotlin/com/goncharov/evgeny/obstacleavoid/systems/collision/CollisionSystem.kt
+++ core/src/main/kotlin/com/goncharov/evgeny/obstacleavoid/systems/collision/CollisionSystem.kt
@@ -1,10 +1,44 @@
 package com.goncharov.evgeny.obstacleavoid.systems.collision
 
+import com.badlogic.ashley.core.Entity
 import com.badlogic.ashley.core.EntitySystem
+import com.badlogic.ashley.core.Family
+import com.goncharov.evgeny.obstacleavoid.common.Mappers
+import com.goncharov.evgeny.obstacleavoid.components.BoundsComponent
+import com.goncharov.evgeny.obstacleavoid.components.ObstacleComponent
+import com.goncharov.evgeny.obstacleavoid.components.PlayerComponent
 
 class CollisionSystem(
     private val listener: CollisionListener
 ) : EntitySystem() {
 
+    private val playerFamily = Family.all(
+        PlayerComponent::class.java,
+        BoundsComponent::class.java
+    ).get()
 
+    private val obstacleFamily = Family.all(
+        ObstacleComponent::class.java,
+        BoundsComponent::class.java
+    ).get()
+
+    override fun update(deltaTime: Float) {
+        val players = engine.getEntitiesFor(playerFamily).first()
+        val obstacles = engine.getEntitiesFor(obstacleFamily)
+        obstacles.forEach { obstacle ->
+            val component = Mappers.obstacle[obstacle]
+            if (component.hit) {
+                return@forEach
+            }
+            if (checkCollision(players, obstacle)) {
+                listener.hitObstacle()
+            }
+        }
+    }
+
+    private fun checkCollision(player: Entity, obstacle: Entity): Boolean {
+        val playerBounds = Mappers.bounds[player]
+        val obstacleBounds = Mappers.bounds[obstacle]
+        return playerBounds.bounds.overlaps(obstacleBounds.bounds)
+    }
 }
