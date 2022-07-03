0db486158bb563a51be8fa55cefe84091381e945
Evgeny Goncharov <jtgn@yandex.ru>
2022-07-03 17:16:03 +0300

    реализация DebugRenderSystem

--- core/src/main/kotlin/com/goncharov/evgeny/obstacleavoid/systems/debug/DebugRenderSystem.kt
+++ core/src/main/kotlin/com/goncharov/evgeny/obstacleavoid/systems/debug/DebugRenderSystem.kt
@@ -3,8 +3,10 @@ package com.goncharov.evgeny.obstacleavoid.systems.debug
 import com.badlogic.ashley.core.Entity
 import com.badlogic.ashley.core.Family
 import com.badlogic.ashley.systems.IteratingSystem
+import com.badlogic.gdx.graphics.Color
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer
 import com.badlogic.gdx.utils.viewport.Viewport
+import com.goncharov.evgeny.obstacleavoid.common.Mappers
 import com.goncharov.evgeny.obstacleavoid.components.BoundsComponent
 
 class DebugRenderSystem(
@@ -14,7 +16,17 @@ class DebugRenderSystem(
     Family.all(BoundsComponent::class.java).get()
 ) {
 
-    override fun processEntity(entity: Entity, deltaTime: Float) {
+    override fun update(deltaTime: Float) {
+        gameViewport.apply()
+        renderer.projectionMatrix = gameViewport.camera.combined
+        renderer.begin(ShapeRenderer.ShapeType.Line)
+        renderer.color = Color.RED
+        super.update(deltaTime)
+        renderer.end()
+    }
 
+    override fun processEntity(entity: Entity, deltaTime: Float) {
+        val bc = Mappers.bounds[entity]
+        renderer.circle(bc.bounds.x, bc.bounds.y, bc.bounds.radius, 30)
     }
 }
